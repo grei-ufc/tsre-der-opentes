77d3a9a139d9e440aa08459f943279d355f3a110
Paulo Victor <pvictor.lima@alu.ufc.br>
2025-11-22 11:17:52 -0300

    Correção de erro no mosaik pandapower na geração dos dados

--- src/data/CriarRede.py
+++ src/data/CriarRede.py
@@ -9,6 +9,16 @@ import os
 net = sb.get_simbench_net('1-LV-urban6--0-sw')
 output_file = '1-LV-urban6--0-sw.json'
 
+# 2. Aumentar MUITO a impedância do transformador
+#net.trafo.at[0, 'vk_percent'] = 20.0  # Impedância extremamente alta
+#net.trafo.at[0, 'vkr_percent'] = 4
+#net.trafo.at[0, 'tap_pos'] = 0
+print(f"Impedância do transformador: {net.trafo.at[0, 'vk_percent']}%")
+print(f"Impedância do transformador: {net.trafo.at[0, 'vkr_percent']}%")
+
+#pp.runpp(net)
+
+
 # # # Salvar a rede em JSON
 pp.to_json(net, output_file)
 
